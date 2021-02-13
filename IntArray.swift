51165b2cfaeea20798242acb0f131f795e540d0b
Franklin <franklin@Franklins-iMac.attlocal.net>
2021-02-13 06:00:04 -0800

    broken ass playground smh

---
+++ IntArray.swift
@@ -0,0 +1,55 @@
+//
+//  IntArray.swift
+//  
+//
+//  Created by Franklin on 2/13/21.
+//
+
+import Cocoa
+
+fileprivate class IntArrayBuffer {
+    var storage: [Int]
+    
+    init() {
+        storage = []
+    }
+    
+    init(buffer: IntArrayBuffer){
+        storage = buffer.storage
+    }
+    
+    struct IntArray {
+        private var buffer: IntArrayBuffer
+        
+        init() {
+            bbuffer = IntArrayBuffer()
+        }
+        
+        func describe(){
+            print(buffer.storage)
+        }
+        
+        func insert(_ value: Int, at index: Int){
+            buffer.storage.insert(value, at: index)
+        }
+        
+        func append(_ value: Int){
+            buffer.storage.append(value)
+        }
+        
+        func remove(at index: Int){
+            buffer.storage.remove(at: index)
+        }
+    }
+    
+    var integers = IntArray()
+    integers.append(1)
+    integers.append(2)
+    integers.append(4)
+    integers.describe()
+    var moreIntegers = integers
+    moreIntegers.insert(3, at: 2)
+    integers.describe()
+    moreIntegers.describe()
+    
+}
