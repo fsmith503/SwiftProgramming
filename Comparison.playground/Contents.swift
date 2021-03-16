7c41b56b9046297f137ff82949c61acff318a265
Franklin <franklin@Franklins-iMac.attlocal.net>
2021-03-15 19:27:37 -0700

    updating

---
+++ Comparison.playground/Contents.swift
@@ -0,0 +1,44 @@
+import Cocoa
+
+//var str = "Hello, playground"
+
+
+struct Point {
+    let x: Int
+    let y: Int
+    let label: String? = nil
+    
+}
+
+extension Point: Comparable {
+    static func ==(lhs: Point, rhs: Point) -> Bool {
+        return (lhs.x == rhs.x) && (lhs.y == rhs.y)
+    }
+    
+    static func <(lhs: Point, rhs: Point) -> Bool {
+        return (lhs.x < rhs.x) && (lhs.y < rhs.y)
+    }
+    
+}
+
+let a = Point(x:3, y: 4)
+let b = Point(x:3, y: 4)
+
+
+let abEqual = (a == b )
+let abNotEqual = (a != b)
+
+let c = Point(x: 2, y: 6)
+let d = Point(x: 3, y: 7)
+let cdEqual = (c == d)
+let cLessThanD = (c < d)
+
+
+
+let cLessThanEqualD = (c <= d)
+let cGreaterThanD = (c < d)
+let cGreaterThanEqualD = (c >= d)
+
+let pointRange = c..<d
+pointRange.contains(a)
+pointRange.contains(Point(x: -1, y: -1))
