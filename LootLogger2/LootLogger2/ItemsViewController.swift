e869c7f547d5410434b7bce6518a1752fe10593f
Franklin Smith <franklin@Mac-306C38.attlocal.net>
2021-09-21 09:04:54 -0700

    updating

--- LootLogger2/LootLogger2/ItemsViewController.swift
+++ LootLogger2/LootLogger2/ItemsViewController.swift
@@ -11,6 +11,29 @@ class ItemsViewController: UITableViewController {
     
     var itemStore: ItemStore!
     
+    @IBAction func addNewItem(_ sender: UIButton){
+        
+    }
+    
+    @IBAction func toggleEditingMode(_ sender: UIButton){
+        
+        // if you are currently in editing mode...
+        if isEditing {
+            // Change text of button to inform user of state
+            sender.setTitle("Edit", for: .normal)
+            // Turn off editing mode
+            setEditing(false, animated: true)
+        } else {
+            // change text of button to inform user of state
+            sender.setTitle("Done", for: .normal)
+            
+            // Enter editing mode
+            setEditing(true, animated: true)
+        }
+        
+    }
+    
+    
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemStore.allItems.count
@@ -19,7 +42,10 @@ class ItemsViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         // Create an instnace of UITableViewCell with default appearance
-        let cell = UITableViewCell(style: .value1, reuseIdentifier: "UITableViewCell")
+        //let cell = UITableViewCell(style: .value1, reuseIdentifier: "UITableViewCell")
+        
+        // Get a new or recycled cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell", for: indexPath)
         
         // Set the text on the cell with the description of the item
         // that is at the nth index of items, where n = row this cell
